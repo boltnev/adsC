b17fdb0db7469e24a494ee8315760fc0eca367c0
Ilya Boltnev <ilya@boltnev.com>
2013-02-03 23:15:01 +0400

    unnecessary files deleted

--- c/main.c
+++ c/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "ds/CFIFOBuffer.h"
+#include "algs/mergesort.h"
+#include "dataloader/dataloader.h"
 
-int main()
-{
-    printf("Hello world!\n");
+#define MAX_ELEMENTS 1024*1024
+
+int main(){
+    int i;
+
+    int datalength;
+    long *data = malloc(sizeof(long) * MAX_ELEMENTS);
+
+    dataload(data, &datalength);
+
+    printf("\n\n%d\n\n", datalength);
+
+    mergesort(data, datalength);
+
+    for(i = 0; i < datalength; i++)
+        printf("%ld ", data[i]);
+
     return 0;
 }
