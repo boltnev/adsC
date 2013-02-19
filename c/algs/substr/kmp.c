be8f016768d8d6da3f2a1858f969280a62eb55c6
Ilya Boltnev <ilya@boltnev.com>
2013-02-19 23:02:20 +0400

    Implemented simple algorithm of substring finding

---
+++ c/algs/substr/kmp.c
@@ -0,0 +1,9 @@
+//
+//  kmp.c
+//  ads
+//
+//  Created by Ilya Boltnev on 2/19/13.
+//  Copyright (c) 2013 Ilya Boltnev. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ c/algs/substr/kmp.h
@@ -0,0 +1,14 @@
+//
+//  kmp.h
+//  ads
+//
+//  Created by Ilya Boltnev on 2/19/13.
+//  Copyright (c) 2013 Ilya Boltnev. All rights reserved.
+//
+
+#ifndef ads_kmp_h
+#define ads_kmp_h
+
+
+
+#endif
